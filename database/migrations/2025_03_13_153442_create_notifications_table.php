fd6840f6fc0e8421e6c51cc6bcafac52752c97b9
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 15:47:14 +0200

    Add notifications migration table columns

--- database/migrations/2025_03_13_153442_create_notifications_table.php
+++ database/migrations/2025_03_13_153442_create_notifications_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('message');
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
