8560d87d9400fab758008dd91fd6f05682aa76f7
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 15:22:34 +0200

    Add role & profile pic url fields to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['employer', 'candidate', 'admin']);
+            $table->text('profile_picture_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
