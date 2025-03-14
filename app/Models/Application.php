9a533c324c7281e87eb98d679f34bfa2b463dfc5
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 17:26:02 +0200

    Add Application model relationship

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -9,4 +9,14 @@ class Application extends Model
 {
     /** @use HasFactory<\Database\Factories\ApplicationFactory> */
     use HasFactory;
+
+    public function jobListing()
+    {
+        return $this->belongsTo(JobListing::class);
+    }
+
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class);
+    }
 }
