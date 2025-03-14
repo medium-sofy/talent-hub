cc987f227ef18f0014019a398cc00c8e048cdc9f
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 15:30:55 +0200

    Add joblisting table migration columns

--- database/migrations/2025_03_13_150847_create_joblistings_table.php
+++ database/migrations/2025_03_13_150847_create_joblistings_table.php
@@ -13,6 +13,25 @@ return new class extends Migration
     {
         Schema::create('joblistings', function (Blueprint $table) {
             $table->id();
+            $table->id();
+            $table->unsignedBigInteger('employer_id');
+            $table->string('title');
+            $table->text('description');
+            $table->text('requirements');
+            $table->text('benefits')->nullable();
+            $table->string('location');
+            $table->unsignedBigInteger('category_id');
+            $table->enum('workplace', ['remote', 'onsite', 'hybrid']);
+            $table->enum('job_type', ['Full-time', 'Part-time', 'freelance']);
+            $table->unsignedInteger('upper_salary')->nullable();
+            $table->unsignedInteger('lower_salary')->nullable();
+            $table->dateTime('application_deadline')->nullable();
+            $table->boolean('is_approved')->default(false);
+            $table->timestamps();
+
+            $table->foreign('employer_id')->references('id')->on('employers')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
