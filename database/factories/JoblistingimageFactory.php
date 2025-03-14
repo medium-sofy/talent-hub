58dc16c626f7345c4f7343dce999ad48eda6a15e
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 16:49:22 +0200

    Add NotificationFactory elements + import required models

--- database/factories/JoblistingimageFactory.php
+++ database/factories/JoblistingimageFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Joblisting;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Joblistingimage>
