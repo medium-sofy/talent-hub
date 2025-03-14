6a337e726e65c32a59740243c0e895bdeeac9423
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 15:25:02 +0200

    Add employers table migration columns

--- database/migrations/2025_03_13_135340_create_employers_table.php
+++ database/migrations/2025_03_13_135340_create_employers_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('employers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('company_name');
+            $table->text('company_logo_url')->nullable();
+            $table->string('website')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
