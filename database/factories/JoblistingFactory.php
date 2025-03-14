ba2568fcff785ab382479d2112212c4d0ef67e74
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 16:43:37 +0200

    Add JoblistingFactory elements

--- database/factories/JoblistingFactory.php
+++ database/factories/JoblistingFactory.php
@@ -17,7 +17,19 @@ class JoblistingFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'requirements' => $this->faker->paragraph,
+            'benefits' => $this->faker->paragraph,
+            'location' => $this->faker->city,
+            'category_id' => Category::factory(),
+            'workplace' => $this->faker->randomElement(['remote', 'onsite', 'hybrid']),
+            'job_type' => $this->faker->randomElement(['Full-time', 'Part-time', 'freelance']),
+            'upper_salary' => $this->faker->numberBetween(12000, 25000),
+            'lower_salary' => $this->faker->numberBetween(5000, 10000),
+            'application_deadline' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'is_approved' => $this->faker->boolean,
         ];
     }
 }
