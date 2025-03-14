d6b331e27f796624d69c62ff094a20d494b59369
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-14 17:03:58 +0200

    Add table factories to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,17 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Analytic;
+use App\Models\Application;
+use App\Models\Candidate;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Employer;
+use App\Models\JobListing;
+use App\Models\JobListingImage;
+use App\Models\Notification;
+use App\Models\Skill;
+use App\Models\Technology;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,13 +22,41 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
+        User::factory(10)->create()->each(function ($user) {
+            // Employers & Candidates (Randomly assign some users)
+            if (rand(0, 1)) {
+                Employer::factory()->create(['user_id' => $user->id]);
+            } else {
+                Candidate::factory()->create(['user_id' => $user->id]);
+            }
+        });
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Category::factory(5)->create();
+
+        Technology::factory(10)->create();
+
+        Skill::factory(15)->create();
+
+        JobListing::factory(20)->create()->each(function ($jobListing) {
+            // Job Listing Skills (Pivot)
+            $skills = Skill::inRandomOrder()->take(rand(2, 5))->pluck('id');
+            $jobListing->skills()->attach($skills);
+
+            // Job Listing Technologies (Pivot)
+            $technologies = Technology::inRandomOrder()->take(rand(1, 3))->pluck('id');
+            $jobListing->technologies()->attach($technologies);
+
+            JobListingImage::factory(rand(1, 3))->create(['job_listing_id' => $jobListing->id]);
+
+            Analytic::factory()->create(['job_listing_id' => $jobListing->id]);
+        });
+
+        Application::factory(30)->create();
+
+        Comment::factory(50)->create();
+
+        Notification::factory(20)->create();
     }
 }
