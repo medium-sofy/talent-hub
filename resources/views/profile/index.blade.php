3680273a06424f6f9cbf428454a3f916da8b1d10
amrah0 <amroa333@gmail.com>
2025-03-17 03:28:58 +0200

    section card class

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -33,7 +33,6 @@
 
     <div class="row">
         <div class="col-md-8">
-            <!-- About Section -->
             <div class="section-card p-4 mb-4">
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h4>About</h4>
