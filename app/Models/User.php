52b4d9f5e5cd2bf254936e94a0cbe68dbc438951
amrah0 <amroa333@gmail.com>
2025-03-17 03:38:30 +0200

    update to singular

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
         return $this->hasOne(Employer::class);
     }
 
-    public function candidates()
+    public function candidate()
     {
         return $this-> hasOne(Candidate::class);
     }
