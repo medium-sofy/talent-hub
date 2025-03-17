f193f3e0a88ea3a6f6b0dd0af07f928d6aaa2a11
Ahellakhaled <ahella>
2025-03-18 01:27:50 +0200

    edit in view

--- resources/views/applications/cand_index.blade.php
+++ resources/views/applications/cand_index.blade.php
@@ -24,10 +24,10 @@
                 <p class="text-gray-700 mb-2">
                     <strong>Applied On:</strong> {{ $application->created_at->format('M d, Y') }}
                 </p>
-                <p class="text-gray-700 mb-4">
+                <!-- <p class="text-gray-700 mb-4">
                     <strong>Resume:</strong>
                     <a href="{{ asset('storage/' . $application->resume_url) }}" target="_blank" class="text-blue-500 hover:text-blue-700 hover:underline">Download Resume</a>
-                </p>
+                </p> -->
 
 
                 <button onclick="openModal('modal-{{ $application->id }}')" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300">
