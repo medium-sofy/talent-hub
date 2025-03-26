5afb6ffd1c88cb537ea60edec0d2b34d356f68b8
amrah0 <amroa333@gmail.com>
2025-03-26 12:15:41 +0200

    adding slug to model

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -2,28 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Candidate extends Model
 {
-    /** @use HasFactory<\Database\Factories\CandidateFactory> */
-    use HasFactory;
-
-    protected $guarded = [];
     protected $fillable = [
         'user_id',
-        'phone_number',
+        'resume_url',
         'linkedin_profile',
-        'resume_url'
+        'phone_number',
+        'slug'
     ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function application()
-    {
-        return $this->hasMany(application::class);
     }
 }
